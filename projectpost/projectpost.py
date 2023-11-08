0916c88d860d1df342eda97610b42b08218d468b
nottherealtar <67793100+nottherealtar@users.noreply.github.com>
2023-11-08 17:45:24 +0200

    Trying to fix message deletion.

--- projectpost/projectpost.py
+++ projectpost/projectpost.py
@@ -23,10 +23,7 @@ class ProjectPost(commands.Cog):
         await ctx.message.delete()
 
         if is_github == "yes":
-            # Ask for the GitHub thumbnail
-            question1 = await ctx.send("Please provide the **GitHub thumbnail URL**:")
-            github_thumbnail_message = await self.bot.wait_for("message", check=lambda m: m.author == ctx.author)
-            github_thumbnail = github_thumbnail_message.content
+            github_thumbnail = "https://cdn.discordapp.com/attachments/1170989523895865424/1171787440583872512/Github.png"
 
             # Delete the user's input messages and the bot's questions
             await question1.delete()
@@ -38,13 +35,19 @@ class ProjectPost(commands.Cog):
         question2 = await ctx.send("What's your **name**?")
         author_name = await self.bot.wait_for("message", check=lambda m: m.author == ctx.author)
 
+        await question2.delete()
+
         # Ask for the project title
         question3 = await ctx.send("What's the **project title**?")
         project_title = await self.bot.wait_for("message", check=lambda m: m.author == ctx.author)
 
+        await question3.delete()
+
         # Ask for the URL to the project
         question4 = await ctx.send("What's the **URL to the project**?")
         project_url = await self.bot.wait_for("message", check=lambda m: m.author == ctx.author)
+
+        await question4.delete()
 
         # Create the timestamp for the current time
         timestamp = datetime.now()
@@ -57,9 +60,6 @@ class ProjectPost(commands.Cog):
         embed.set_footer(text=f"Posted at {timestamp}")
 
         # Delete the user's input messages and the bot's questions
-        await question2.delete()
-        await question3.delete()
-        await question4.delete()
         await ctx.message.delete()
 
         # Send the nicely formatted embed to the channel
