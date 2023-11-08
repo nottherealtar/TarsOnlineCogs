3492c4ce6a90cf87912e10311f67d1f2167242a5
nottherealtar <67793100+nottherealtar@users.noreply.github.com>
2023-11-08 10:10:27 +0200

    TEST

--- assky/assky.py
+++ assky/assky.py
@@ -1,38 +1,25 @@
 import random
 import aiohttp
 import discord
-from redbot.core import commands, app_commands
-import os
+from redbot.core import commands
+import pyfiglet
 
 class AsSky(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.base_url = "http://artii.herokuapp.com/fonts_list"
-        self.fonts = []
-
-    @commands.Cog.listener()
-    async def on_ready(self):
-        async with aiohttp.ClientSession() as session:
-            async with session.get(self.base_url) as response:
-                if response.status == 200:
-                    data = await response.text()
-                    self.fonts = data.split('\n')
-                else:
-                    print("Error retrieving font list")
+        self.fonts = pyfiglet.FigletFont.getFonts()
 
     @commands.command()
     async def assky(self, ctx, *, text: str):
-        if len(self.fonts) == 0:
+        if not self.fonts:
             await ctx.send("No fonts found.")
             return
 
         random_font = random.choice(self.fonts)
-        base_url = f"http://artii.herokuapp.com/make?text={text}&font={random_font}"
 
-        async with aiohttp.ClientSession() as session:
-            async with session.get(base_url) as response:
-                if response.status == 200:
-                    data = await response.text()
-                    await ctx.send(f"```\n{data}\n```")
-                else:
-                    await ctx.send("Error generating ASCII art.")
+        try:
+            custom_fig = pyfiglet.Figlet(font=random_font)
+            ascii_art = custom_fig.renderText(text)
+            await ctx.send("```\n" + ascii_art + "\n```")
+        except pyfiglet.FontNotFound:
+            await ctx.send("Font not found")
