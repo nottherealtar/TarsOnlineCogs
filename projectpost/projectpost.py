69487638f7ad6399330f94f2dcc2ca069300ddee
nottherealtar <67793100+nottherealtar@users.noreply.github.com>
2023-11-08 14:33:23 +0200

    Added better formatting

--- projectpost/projectpost.py
+++ projectpost/projectpost.py
@@ -9,15 +9,15 @@ class ProjectPost(commands.Cog):
     @commands.command()
     async def projectpost(self, ctx):
         # Ask for the user's name
-        await ctx.send("What's your name?")
+        question1 = await ctx.send("What's your **name**?")
         author_name = await self.bot.wait_for("message", check=lambda m: m.author == ctx.author)
 
         # Ask for the project title
-        await ctx.send("What's the project title?")
+        question2 = await ctx.send("What's the **project title**?")
         project_title = await self.bot.wait_for("message", check=lambda m: m.author == ctx.author)
 
         # Ask for the URL to the project
-        await ctx.send("What's the URL to the project?")
+        question3 = await ctx.send("What's the **URL to the project**?")
         project_url = await self.bot.wait_for("message", check=lambda m: m.author == ctx.author)
 
         # Create the timestamp for the current time
@@ -26,8 +26,9 @@ class ProjectPost(commands.Cog):
         # Check if the project URL is from GitHub
         is_github = project_url.content.startswith("https://github.com/")
 
-        # Create the embed with or without a thumbnail
-        embed = Embed(title=project_title.content, url=project_url.content, description=f"Author: {author_name.content}")
+        # Create the nicely formatted embed
+        embed = Embed(title=project_title.content, url=project_url.content)
+        embed.description = f"**Author:** {author_name.content}"
         embed.set_footer(text=f"Posted at {timestamp}")
 
         # If the URL is from GitHub, add a thumbnail
@@ -35,10 +36,10 @@ class ProjectPost(commands.Cog):
             embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/1170989523895865424/1171787440583872512/Github.png")
 
         # Delete the user's input messages and the bot's questions
+        await question1.delete()
+        await question2.delete()
+        await question3.delete()
         await ctx.message.delete()
-        await author_name.delete()
-        await project_title.delete()
-        await project_url.delete()
 
-        # Send the embed to the channel
+        # Send the nicely formatted embed to the channel
         await ctx.send(embed=embed)
