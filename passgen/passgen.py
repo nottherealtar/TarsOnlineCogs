3f3bc178d707fa324bb9bc4d0da4e3a5babd74fa
nottherealtar <67793100+nottherealtar@users.noreply.github.com>
2023-11-19 23:39:41 +0200

    moved away from the buttons and switched to user input

--- passgen/passgen.py
+++ passgen/passgen.py
@@ -15,47 +15,25 @@ import string
 
 class PassGen(commands.Cog):
 
-  def __init__(self, bot):
+ def __init__(self, bot):
     self.bot = bot
 
-  @commands.command()
-  async def passgen(self, ctx):
-    """Generates a random password"""
+ @commands.command()
+ async def passgen(self, ctx, length: int = 8):
+    """Generates a random password of the specified length"""
 
-    try:
-      view = PasswordLengthView(ctx, self.bot)
-      await ctx.send("Choose password length:", view=view)
-    except Exception as e:
-      await ctx.send(f"Error generating password: {e}")
+    if length < 1:
+      await ctx.send("Password length must be at least 1.")
+      return
 
-
-class PasswordLengthView(discord.ui.View):
-
-  def __init__(self, ctx, bot):
-    super().__init__()
-    self.ctx = ctx
-    self.bot = bot
-
-  @discord.ui.button(label="8 Characters")
-  async def generate_password_8(self, button, interaction):
-    
-    length = 8
     password = self._generate_password(length)
 
-    # Handle AttributeError on interaction
     try:
-      ctx = await self.bot.get_context(interaction)
-    except AttributeError: 
-      ctx = None
-    
-    try:
-      await interaction.user.send(f"Here is your password: {password}") 
-    except Exception as e:
-      if ctx:
-        await ctx.send(f"Error sending DM: {e}")
+      await ctx.author.send(f"Here is your password: {password}")
+      await ctx.send("I have sent you a DM with your password.")
+    except discord.Forbidden:
+      await ctx.send(f"I couldn't send you a DM. Here is your password: {password}")
 
-  # Other button methods
-
-  def _generate_password(self, length):
+ def _generate_password(self, length):
     characters = string.ascii_letters + string.digits
     return ''.join(random.choice(characters) for i in range(length))
