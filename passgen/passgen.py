859c6c4313f86d319e37ab704b9b116c029f01ff
nottherealtar <67793100+nottherealtar@users.noreply.github.com>
2023-11-19 21:11:12 +0200

    Syntax errors are killing me

--- passgen/passgen.py
+++ passgen/passgen.py
@@ -1,102 +1,65 @@
-import string
-import random
+# passgen.py
+
+import discord
 from redbot.core import commands
+import random
+import string
+from discord import ext_command
 
 class Passgen(commands.Cog):
+
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
     async def passgen(self, ctx):
-        """
-        Generates a random password based on user input.
-        """
-        try:
-            # Ask for the user's desired password length
-            await ctx.send("Please select the desired length for your password:")
+        """Generates a random password"""
+        
+        # Create embed
+        embed = discord.Embed(title="Password Generator", color=discord.Color.green())
+        
+        # Add length select menu
+        length_select = ["6", "8", "10", "12", "16", "20", "24", "32"] 
+        length_menu = discord.ui.Select(placeholder="Select password length", options=[discord.SelectOption(label=i, value=i) for i in length_select])
+        length_menu.callback = self.length_callback
+        
+        # Add symbol select menu
+        symbol_select = ["Basic", "Advanced"]
+        symbol_menu = discord.ui.Select(placeholder="Select symbol type", options=[discord.SelectOption(label=i, value=i) for i in symbol_select])
+        symbol_menu.callback = self.symbol_callback
+        
+        # Send embed with menus
+        view = discord.ui.View()
+        view.add_item(length_menu)
+        view.add_item(symbol_menu)
+        await ctx.send(embed=embed, view=view)
 
-            # Create the buttons with options for password length
-            buttons = [
-                Button(style=ButtonStyle.blue, label=str(i), custom_id=f"passgen_length_{i}")
-                for i in range(6, 33)
-            ]
+    async def length_callback(self, interaction):
+        # Store selected length
+        self.length = int(interaction.data['values'][0])
+        await interaction.response.defer()
 
-            # Create a view to handle button interactions
-            view = PassgenView(timeout=60)
+    async def symbol_callback(self, interaction):  
+        # Store selected symbols
+        self.symbols = interaction.data['values'][0]
+        await interaction.response.defer()
 
-            # Add the buttons to the view
-            for button in buttons:
-                view.add_item(button)
+        # Generate password
+        password = self.generate_password()
+        
+        # Send password in DM
+        await interaction.user.send(f"Here is your generated password: `{password}`")
+        
+        # Delete original message
+        await interaction.message.delete()
 
-            # Send the buttons and view
-            await ctx.send("Choose password length:", view=view)
-
-            # Wait for user interaction
-            interaction = await view.wait()
-
-            # Get the selected length from the custom_id
-            password_length = int(interaction.custom_id.split("_")[-1])
-
-            # Ask for basic or advanced symbols
-            await interaction.response.send_message(f"You selected a password length of {password_length}. Now, choose symbol options:")
-
-            # Create the buttons with options for symbol types
-            buttons = [
-                Button(style=ButtonStyle.green, label="Basic Symbols", custom_id="passgen_basic"),
-                Button(style=ButtonStyle.red, label="Advanced Symbols", custom_id="passgen_advanced"),
-            ]
-
-            # Create a new view to handle button interactions
-            view = PassgenView(timeout=60)
-
-            # Add the buttons to the view
-            for button in buttons:
-                view.add_item(button)
-
-            # Send the buttons and view
-            await interaction.response.edit_message(content="Choose symbol options:", view=view)
-
-            # Wait for user interaction
-            interaction = await view.wait()
-
-            # Get the selected symbol type from the custom_id
-            use_advanced_symbols = "advanced" in interaction.custom_id
-
-            # Generate the password based on user choices
-            password_characters = string.ascii_letters + string.digits
-            if use_advanced_symbols:
-                password_characters += string.punctuation
-
-            generated_password = ''.join(random.choice(password_characters) for _ in range(password_length))
-
-            # Send the generated password via direct message
-            await ctx.author.send(f"Here is your generated password: `{generated_password}`")
-
-            # Clean up traces
-            await interaction.response.delete_message()
-
-        except TimeoutError:
-            await ctx.send("Time limit exceeded. Please run the command again.")
-
-        except ValueError:
-            await ctx.send("Invalid input. Please run the command again and choose a valid option.")
-
-        except Exception as e:
-            # Handle other errors
-            await ctx.send(f"An error occurred: {str(e)}")
-
-class PassgenView(ext_commands.View):
-    def __init__(self, *, timeout=180):
-        super().__Init__(timeout=timeout) # Initializing view with timeout
-
-    async def on_timeout(self):
-        for item in self.children:
-            item.disabled = True # Disable buttons on timeout
-
-        await self.response.edit(content="Timeout reached. Please run the command again.", view=self) # Edit message on timeout
-
-    async def on_error(self, error, item, interaction):
-        for item in self.children:
-            item.disabled = True # Disable buttons on error
-
-        await self.response.edit(content=f"An error occurred: {str(error)}", view=self) # Edit message on error
+    def generate_password(self):
+        # Generate password using selected options
+        chars = string.ascii_letters + string.digits
+        if self.symbols == "Basic":
+            chars += "!@#$%^&*()"
+        elif self.symbols == "Advanced":
+            chars += "!@#$%^&*()_+=-{}[]:;'<>?,./|\\"
+            
+        password = ''.join(random.choice(chars) for i in range(self.length))
+        return password
