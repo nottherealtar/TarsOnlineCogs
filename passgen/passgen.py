d9790b1fafc5702ab66d9ab3542675008c572306
nottherealtar <67793100+nottherealtar@users.noreply.github.com>
2023-11-19 23:16:32 +0200

    Attributes now access the bot instance directly

--- passgen/passgen.py
+++ passgen/passgen.py
@@ -23,7 +23,7 @@ class PassGen(commands.Cog):
         """Generates a random password"""
 
         try:
-            view = PasswordLengthView(ctx)
+            view = PasswordLengthView(ctx, self.bot)
             await ctx.send("Choose password length:", view=view, delete_after=10)
         except Exception as e:
             await ctx.send(f"Error generating password: {e}")
@@ -31,9 +31,10 @@ class PassGen(commands.Cog):
 
 class PasswordLengthView(discord.ui.View):
 
-    def __init__(self, ctx):
+    def __init__(self, ctx, bot):
         super().__init__()
         self.ctx = ctx
+        self.bot = bot
 
     @discord.ui.button(label="8 Characters", style=discord.ButtonStyle.primary)
     async def generate_password_8(self, button: discord.ui.Button, interaction: discord.Interaction):
@@ -44,13 +45,13 @@ class PasswordLengthView(discord.ui.View):
         ctx = await self.bot.get_context(interaction)
         
         try:
-            await interaction.user.send(f"Here is your {length} character password: `{password}`") 
+            await interaction.user.send(f"Here is your {length} character password: `{password}`")
         except Exception as e:
             await ctx.send(f"Error sending DM: {e}", ephemeral=True)
 
     @discord.ui.button(label="16 Characters", style=discord.ButtonStyle.primary)
     async def generate_password_16(self, button: discord.ui.Button, interaction: discord.Interaction):
-
+        
         length = 16
         password = self._generate_password(length)
 
@@ -64,3 +65,4 @@ class PasswordLengthView(discord.ui.View):
     def _generate_password(self, length):
         characters = string.ascii_letters + string.digits
         return ''.join(random.choice(characters) for i in range(length))
+
