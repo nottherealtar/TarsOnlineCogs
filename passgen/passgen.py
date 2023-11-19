53ca160b3c506fffa17a58ef787b82bd47084312
nottherealtar <67793100+nottherealtar@users.noreply.github.com>
2023-11-19 23:45:07 +0200

    Added formatting for the password adding descriptions now aswell

--- passgen/passgen.py
+++ passgen/passgen.py
@@ -7,7 +7,6 @@
 # 
 
 # Import statements 
-
 from redbot.core import commands
 import discord
 import random
@@ -15,25 +14,46 @@ import string
 
 class PassGen(commands.Cog):
 
- def __init__(self, bot):
-    self.bot = bot
+    def __init__(self, bot):
+        self.bot = bot
 
- @commands.command()
- async def passgen(self, ctx, length: int = 8):
-    """Generates a random password of the specified length"""
+    @commands.command()
+    async def passgen(self, ctx):
+        """Generates a random password of the specified length"""
 
-    if length < 1:
-      await ctx.send("Password length must be at least 1.")
-      return
+        def check(m):
+            return m.author == ctx.author and m.channel == ctx.channel
 
-    password = self._generate_password(length)
+        try:
+            await ctx.author.send("Please enter a password length between 6 and 32.")
+        except discord.Forbidden:
+            await ctx.send("I couldn't send you a DM. Please enter a password length between 6 and 32.")
+            return
 
-    try:
-      await ctx.author.send(f"Here is your password: {password}")
-      await ctx.send("I have sent you a DM with your password.")
-    except discord.Forbidden:
-      await ctx.send(f"I couldn't send you a DM. Here is your password: {password}")
+        while True:
+            try:
+                msg = await self.bot.wait_for('message', check=check, timeout=30.0)
+            except asyncio.TimeoutError:
+                await ctx.send('Timeout reached, please run the command again.')
+                return
 
- def _generate_password(self, length):
-    characters = string.ascii_letters + string.digits
-    return ''.join(random.choice(characters) for i in range(length))
+            try:
+                length = int(msg.content)
+                if length < 6 or length > 32:
+                    await ctx.send("Invalid length. Please enter a password length between 6 and 32.")
+                    continue
+                break
+            except ValueError:
+                await ctx.send("Invalid input. Please enter a number.")
+
+        password = self._generate_password(length)
+
+        try:
+            await ctx.author.send(f"Here is your password: ``{password}``")
+            await ctx.send("I have sent you a DM with your password.")
+        except discord.Forbidden:
+            await ctx.send(f"I couldn't send you a DM. Here is your password: {password}")
+
+    def _generate_password(self, length):
+        characters = string.ascii_letters + string.digits
+        return ''.join(random.choice(characters) for i in range(length))
