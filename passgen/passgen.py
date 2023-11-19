17552774a356bdb1b127cbe2f44d51ceff28cef7
nottherealtar <67793100+nottherealtar@users.noreply.github.com>
2023-11-19 22:53:28 +0200

    Fixed certain functions not joining the strings + formatting for the dsicord button i guess

--- passgen/passgen.py
+++ passgen/passgen.py
@@ -6,41 +6,35 @@
 # |_| \_|\___/ |_|   |_| |_| |_|_____|_| \_\_____/_/   \_\_____|_/_/   \_\_| \_\
 # 
 
-import random
-import string
 from redbot.core import commands
 import discord
-
+import random
+import string
 
 class PassGen(commands.Cog):
+
     def __init__(self, bot):
         self.bot = bot
 
-view = discord.ui.View()
-chars = ("!@#")
+    @commands.command()
+    async def passgen(self, ctx):
+        """Generates a random password"""
+        
+        view = PasswordLengthView()
+        await ctx.send("Choose password length:", view=view)
 
-@commands.command()
-async def passgen(self, ctx):
+class PasswordLengthView(discord.ui.View):
 
-    async def eight_char(interaction):
-        password = self.generate_password(8)
+    @discord.ui.button(label="8 Characters") 
+    async def eight_char(self, button, interaction):
+        password = generate_password(8)
         await interaction.response.send_message(f"Here is your 8 character password: ``{password}``")
 
-    async def sixteen_char(interaction):
-        password = self.generate_password(16) 
+    @discord.ui.button(label="16 Characters")
+    async def sixteen_char(self, button, interaction):
+        password = generate_password(16)
         await interaction.response.send_message(f"Here is your 16 character password: ``{password}``")
 
-    eight_button = discord.ui.Button(label="8 Characters", row=1)
-    eight_button.callback = eight_char
-
-    sixteen_button = discord.ui.Button(label="16 Characters", row=1)
-    sixteen_button.callback = sixteen_char
-
-    view.add_item(eight_button)
-    view.add_item(sixteen_button)
-
-    await ctx.send("Choose password length:", view=view)
-
-def generate_password(self, length):
-    # Existing password generation code
-    return ''.join(random.choice(chars) for i in range(length)) 
+def generate_password(length):
+    chars = string.ascii_letters + string.digits + "!@#$"
+    return ''.join(random.choice(chars) for i in range(length))
