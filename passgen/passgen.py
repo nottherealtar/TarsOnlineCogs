3e5bcb6438c0ba3a05615403fa9fa3d6ca932932
nottherealtar <67793100+nottherealtar@users.noreply.github.com>
2023-11-19 21:16:37 +0200

    Ok back to basics biatches xdd

--- passgen/passgen.py
+++ passgen/passgen.py
@@ -1,64 +1,20 @@
-# passgen.py
-
-import discord
-from redbot.core import commands
 import random
 import string
+from redbot.core import commands
 
-class PassGen(commands.Cog):
+class Passgen(commands.Cog):
 
-    def __init__(self, bot):
-        self.bot = bot
+    def __init__(self):
+        super().__init__()
+        self.cooldown = commands.CooldownMapping.from_cooldown(15, 17, commands.BucketType.user)
 
     @commands.command()
+    @commands.cooldown(1, 15, commands.BucketType.user)
     async def passgen(self, ctx):
-        """Generates a random password"""
-        
-        # Create embed
-        embed = discord.Embed(title="Password Generator", color=discord.Color.green())
-        
-        # Add length select menu
-        length_select = ["6", "8", "10", "12", "16", "20", "24", "32"] 
-        length_menu = discord.ui.Select(placeholder="Select password length", options=[discord.SelectOption(label=i, value=i) for i in length_select])
-        length_menu.callback = self.length_callback
-        
-        # Add symbol select menu
-        symbol_select = ["Basic", "Advanced"]
-        symbol_menu = discord.ui.Select(placeholder="Select symbol type", options=[discord.SelectOption(label=i, value=i) for i in symbol_select])
-        symbol_menu.callback = self.symbol_callback
-        
-        # Send embed with menus
-        view = discord.ui.View()
-        view.add_item(length_menu)
-        view.add_item(symbol_menu)
-        await ctx.send(embed=embed, view=view)
-
-    async def length_callback(self, interaction):
-        # Store selected length
-        self.length = int(interaction.data['values'][0])
-        await interaction.response.defer()
-
-    async def symbol_callback(self, interaction):  
-        # Store selected symbols
-        self.symbols = interaction.data['values'][0]
-        await interaction.response.defer()
-
-        # Generate password
         password = self.generate_password()
-        
-        # Send password in DM
-        await interaction.user.send(f"Here is your generated password: `{password}`")
-        
-        # Delete original message
-        await interaction.message.delete()
+        await ctx.author.send(f"Here is your password: {password}")
+        await ctx.message.delete()
 
     def generate_password(self):
-        # Generate password using selected options
-        chars = string.ascii_letters + string.digits
-        if self.symbols == "Basic":
-            chars += "!@#$%^&*()"
-        elif self.symbols == "Advanced":
-            chars += "!@#$%^&*()_+=-{}[]:;'<>?,./|\\"
-            
-        password = ''.join(random.choice(chars) for i in range(self.length))
-        return password
+        chars = string.ascii_letters + string.digits + "!@#$%^&*"
+        return ''.join(random.choice(chars) for i in range(8))
