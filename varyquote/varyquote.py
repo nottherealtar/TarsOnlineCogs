e21bb91d550c40fe1b4323899fd2b7c14bc7f53d
nottherealtar <67793100+nottherealtar@users.noreply.github.com>
2023-12-21 10:14:26 +0200

    - varyquote updates, adding better api handling. fixing invalid data.

--- varyquote/varyquote.py
+++ varyquote/varyquote.py
@@ -9,7 +9,7 @@ class VaryQuote(BaseCog):
     """
     A cog that provides random quotes from various categories
     """
-    
+
     def __init__(self, bot):
         self.bot = bot
         self.api_token = None
@@ -31,7 +31,7 @@ class VaryQuote(BaseCog):
         """
         Display the available quote categories
         """
-        categories = ["inspire", "management", "sports", "life", "funny"] 
+        categories = ["age", "alone", "amazing", "anger", "architecture", "art", "attitude", "beauty", "best", "birthday", "business", "car", "change", "communication", "computers", "cool", "courage", "dad", "dating", "death", "design", "dreams", "education", "environmental", "equality", "experience", "failure", "faith", "family", "famous", "fear", "fitness"] 
         embed = discord.Embed(title="Available Categories")
         embed.description = "\n".join(categories)
         await ctx.send(embed=embed)
@@ -41,19 +41,19 @@ class VaryQuote(BaseCog):
         """
         Get a random quote from the specified category
         """
-        token = await self.get_api_token()
-        url = f"https://api.api-ninjas.com/v1/quotes?category={category}"
-        headers = {"Authorization": f"Bearer {token}"}
-        
+        valid_categories = ["inspire", "management", "sports", "life", "funny"] 
+
+        if category not in valid_categories:
+            await ctx.send(f"Invalid category. Use `{ctx.prefix}varyquote list` to view available categories.")
+            return
+
         async with aiohttp.ClientSession() as session:
-            async with session.get(url, headers=headers) as resp:
-                data = await resp.json()
-                
-                if data:
+            async with session.get(f"https://varyquote-api.herokuapp.com/quotes/{category}") as response:
+                if response.status == 200:
+                    data = await response.json()
                     quote = random.choice(data)
-                    
-                    embed = discord.Embed(title=category.capitalize() + " Quote")
-                    embed.add_field(name="Quote", value=quote["quote"], inline=False)
-                    embed.add_field(name="Author", value=quote["author"], inline=False)
-                    
+                    embed = discord.Embed(title=quote["category"], description=quote["quote"])
+                    embed.set_footer(text=quote["author"])
                     await ctx.send(embed=embed)
+                else:
+                    await ctx.send("An error occurred while fetching the quote.")
