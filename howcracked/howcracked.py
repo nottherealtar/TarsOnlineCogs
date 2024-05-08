631a3ab2861d7234e8d52b1de97e18bc2b226ee5
nottherealtar <67793100+nottherealtar@users.noreply.github.com>
2024-05-08 21:29:51 +0200

    .

--- howcracked/howcracked.py
+++ howcracked/howcracked.py
@@ -145,7 +145,8 @@ class HowCracked(commands.Cog):
             await ctx.send(f"No {record_type} record found.")
             return
 
-        record_date = datetime.strptime(record['time'], '%Y-%m-%d %H:%M:%S.%f')
+        #record_date = datetime.strptime(record['time'], '%Y-%m-%d %H:%M:%S.%f')
+        record_date = datetime.fromtimestamp(record['time'])
         days_since_record = (datetime.utcnow() - record_date).days
 
         embed = Embed(title=f"{record_type.capitalize()} Cracked Record", color=0x00ff00)
